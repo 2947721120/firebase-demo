0c3b333978f0fc6d0fe845e50025555f1a019df0
2947721120 <2947721120@users.noreply.github.com>
2016-04-17 20:17:43 +0800

    Added files via upload
    上传图片演示

---
+++ README.txt
@@ -0,0 +1,7 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/y198650322/pen/EKQGvV.
+
+ Upload Images to Firebase and delete
+
+Subir imagenes a firebase y borrarlas
+
+Forked from [Jesus Alejandro Atao](http://codepen.io/jealeat96/)'s Pen [Upload Images to Firebase](http://codepen.io/jealeat96/pen/mPBGbG/).
---
+++ js/index.js
@@ -0,0 +1,55 @@
+var img2fire = angular.module('img2fire', ['firebase', 'angular.filter']);
+img2fire.controller("base64Ctrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://luminous-torch-9179.firebaseio.com/");
+  var img = new Firebase("https://luminous-torch-9179.firebaseio.com/images");
+  $scope.imgs = $firebaseArray(img);
+  var _validFileExtensions = [".jpg", ".jpeg", ".bmp", ".gif", ".png"];
+  $scope.uploadFile = function() {
+    var sFileName = $("#nameImg").val();
+    if (sFileName.length > 0) {
+      var blnValid = false;
+      for (var j = 0; j < _validFileExtensions.length; j++) {
+        var sCurExtension = _validFileExtensions[j];
+        if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+          blnValid = true;
+          var filesSelected = document.getElementById("nameImg").files;
+          if (filesSelected.length > 0) {
+            var fileToLoad = filesSelected[0];
+            var fileReader = new FileReader();
+            fileReader.onload = function(fileLoadedEvent) {
+              var textAreaFileContents = document.getElementById(
+                "textAreaFileContents"
+              );
+              $scope.imgs.$add({
+                date: Firebase.ServerValue.TIMESTAMP,
+                base64: fileLoadedEvent.target.result
+              });
+            };
+            fileReader.readAsDataURL(fileToLoad);
+          }
+          break;
+        }
+      }
+      if (!blnValid) {
+        alert('文件无效');
+        return false;
+      }
+    }
+    return true;
+  }
+  $scope.deleteimg = function(imgid) {
+    var misimg = new Firebase("https://luminous-torch-9179.firebaseio.com/images/" + imgid);
+    var r = confirm("你想删除此图片 ?");
+    if (r == true) {
+      misimg.remove();
+      var onComplete = function(error) {
+        if (error) {
+          console.log('无法上传图片');
+        } else {
+          console.log('该图像是成功上传');
+        }
+      };
+      misimg.remove(onComplete);
+    }
+  }
+});
